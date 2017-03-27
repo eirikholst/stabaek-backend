8ecca3cdc9ae8c1f5b969c90a5b0e20eb0f50172
eirikholst <eirik.holst@bouvet.no>
2017-03-27 09:00:27 +0200

    added @scheduled on init method to call every 24th hour

--- src/main/java/no/bouvet/sandvika/stabaek/controller/AdminController.java
+++ src/main/java/no/bouvet/sandvika/stabaek/controller/AdminController.java
@@ -2,6 +2,7 @@ package no.bouvet.sandvika.stabaek.controller;
 
 import no.bouvet.sandvika.stabaek.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class AdminController {
     public AdminController() {
     }
 
+    @Scheduled(fixedDelay = 86400000)
     @CrossOrigin
     @RequestMapping({"/admin/init"})
     public void init() {
